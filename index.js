fd88c5919a201ea4dddc8a4b697abb0acf8012ce
Matthew Migliore <mmigliore311@gmail.com>
2018-09-13 12:09:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector);
+}
 
+function nestedTarget() {
+  return document.querySelector('#nested .target');
+}
+
+function increaseRankBy(n) {
+  var ranks =  document.querySelectorAll("ul.ranked-list li");
+  for (let i = 0; i < ranks.length; i++) {
+   var intRank = parseInt(ranks[i].innerHTML);
+   ranks[i].innerHTML = intRank + n;
+  }
+}
+
+function deepestChild() {
+  var node = document.querySelector('#grand-node').querySelectorAll('div');
+  var deepest;
+  for (let i = 0; i < node.length; i++) {
+   deepest = node[i].children[0];
+  }
+  return deepest;
+}
